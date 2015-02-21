159ebb4e6482112983f263243256e4cbdaadd072
Alexis Leroux-Chartre <alexis.leroux.chartre@gmail.com>
2015-02-21 13:34:04 -0500

    Clean up

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,32 +4,12 @@ module.exports = function(grunt) {
 	// Project configuration
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		meta: {
-			banner:
-				'/*!\n' +
-				' * reveal.js <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd, HH:MM") %>)\n' +
-				' * http://lab.hakim.se/reveal-js\n' +
-				' * MIT licensed\n' +
-				' *\n' +
-				' * Copyright (C) 2015 Hakim El Hattab, http://hakim.se\n' +
-				' */'
-		},
-
-		uglify: {
-			options: {
-				banner: '<%= meta.banner %>\n'
-			},
-			build: {
-				src: 'js/reveal.js',
-				dest: 'js/reveal.min.js'
-			}
-		},
 
 		sass: {
 			core: {
 				files: {
 					'css/reveal.css': 'css/reveal.scss',
-					'css/custom.css': 'css/custom.scss',
+					'css/custom.css': 'css/custom.scss'
 				}
 			},
 			themes: {
@@ -99,17 +79,6 @@ module.exports = function(grunt) {
 			}
 		},
 
-		zip: {
-			'reveal-js-presentation.zip': [
-				'index.html',
-				'css/**',
-				'js/**',
-				'lib/**',
-				'images/**',
-				'plugin/**'
-			]
-		},
-
 		watch: {
             options: {
                 livereload: true
@@ -134,33 +103,19 @@ module.exports = function(grunt) {
 	});
 
 	// Dependencies
-	grunt.loadNpmTasks( 'grunt-contrib-qunit' );
+    grunt.loadNpmTasks( 'grunt-webpack' );
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
-	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 	grunt.loadNpmTasks( 'grunt-sass' );
 	grunt.loadNpmTasks( 'grunt-contrib-connect' );
 	grunt.loadNpmTasks( 'grunt-autoprefixer' );
-	grunt.loadNpmTasks( 'grunt-zip' );
-
-	// Default task
-	grunt.registerTask( 'default', [ 'css', 'js' ] );
-
-	// JS task
-	grunt.registerTask( 'js', [ 'jshint', 'uglify' ] );
 
 	// Theme CSS
 	grunt.registerTask( 'css-themes', [ 'sass:themes' ] );
 
-	// Core framework CSS
-	grunt.registerTask( 'css-core', [ 'sass:core', 'autoprefixer', 'cssmin' ] );
-
 	// All CSS
 	grunt.registerTask( 'css', [ 'sass', 'autoprefixer', 'cssmin' ] );
-
-	// Package presentation to archive
-	grunt.registerTask( 'package', [ 'default', 'zip' ] );
 
 	// Serve presentation locally
 	grunt.registerTask( 'serve', [ 'connect', 'watch' ] );
--- js/live_editor.js
+++ js/live_editor.js
@@ -1,168 +1,154 @@
-var IS_MOBILE = (
-  navigator.userAgent.match(/Android/i)
-    || navigator.userAgent.match(/webOS/i)
-    || navigator.userAgent.match(/iPhone/i)
-    || navigator.userAgent.match(/iPad/i)
-    || navigator.userAgent.match(/iPod/i)
-    || navigator.userAgent.match(/BlackBerry/i)
-    || navigator.userAgent.match(/Windows Phone/i)
-);
-
 var CodeMirrorEditor = React.createClass({displayName: 'CodeMirrorEditor',
-  componentDidMount: function() {
-    if (IS_MOBILE) return;
+    componentDidMount: function() {
+        hljs.highlightBlock(this.refs.editor.getDOMNode());
+    },
 
-    this.editor = CodeMirror.fromTextArea(this.refs.editor.getDOMNode(), {
-      mode: 'javascript',
-      lineNumbers: false,
-      lineWrapping: true,
-      smartIndent: false,  // javascript mode does bad things with jsx indents
-      matchBrackets: true,
-      theme: 'solarized-light',
-      readOnly: this.props.readOnly,
-      tabWidth: 2
-    });
-    this.editor.on('change', this.handleChange);
-  },
+    componentDidUpdate: function() {
+        if (this.props.readOnly) {
+            //this.editor.setValue(this.props.codeText);
+        }
+    },
 
-  componentDidUpdate: function() {
-    if (this.props.readOnly) {
-      this.editor.setValue(this.props.codeText);
+    handleFocus: function (event) {
+        var codeEl = event.currentTarget.firstChild;
+        codeEl.innerText = codeEl.innerText;
+    },
+
+    handleBlur: function (event) {
+        hljs.highlightBlock( event.currentTarget );
+    },
+
+    handleChange: function(event) {
+        if (!this.props.readOnly) {
+            this.props.onChange && this.props.onChange(event.currentTarget.innerText);
+        }
+    },
+
+    render: function() {
+        var editor = React.createElement("pre", {ref: "editor", style: {overflow: 'scroll' },
+                onKeyUp: this.handleChange,
+                onBlur: this.handleBlur,
+                onFocus: this.handleFocus
+            },
+            React.createElement("code", { className: "javascript", contentEditable: true } , this.props.codeText)
+        );
+
+        return (
+            React.createElement("div", {style: this.props.style, className: this.props.className},
+                editor
+            )
+        );
     }
-  },
-
-  handleChange: function() {
-    if (!this.props.readOnly) {
-      this.props.onChange && this.props.onChange(this.editor.getValue());
-    }
-  },
-
-  render: function() {
-    // wrap in a div to fully contain CodeMirror
-    var editor;
-
-    if (IS_MOBILE) {
-      editor = React.createElement("pre", {style: {overflow: 'scroll'}}, this.props.codeText);
-    } else {
-      editor = React.createElement("textarea", {ref: "editor", defaultValue: this.props.codeText});
-    }
-
-    return (
-      React.createElement("div", {style: this.props.style, className: this.props.className}, 
-        editor
-      )
-    );
-  }
 });
 
 var selfCleaningTimeout = {
-  componentDidUpdate: function() {
-    clearTimeout(this.timeoutID);
-  },
+    componentDidUpdate: function() {
+        clearTimeout(this.timeoutID);
+    },
 
-  setTimeout: function() {
-    clearTimeout(this.timeoutID);
-    this.timeoutID = setTimeout.apply(null, arguments);
-  }
+    setTimeout: function() {
+        clearTimeout(this.timeoutID);
+        this.timeoutID = setTimeout.apply(null, arguments);
+    }
 };
 
 var ReactPlayground = React.createClass({displayName: 'ReactPlayground',
-  mixins: [selfCleaningTimeout],
+    mixins: [selfCleaningTimeout],
 
-  propTypes: {
-    codeText: React.PropTypes.string.isRequired,
-    transformer: React.PropTypes.func
-  },
+    propTypes: {
+        codeText: React.PropTypes.string.isRequired,
+        transformer: React.PropTypes.func
+    },
 
-  getDefaultProps: function() {
-    return {
-      transformer: function(code) {
-        return JSXTransformer.transform(code, { harmony: true }).code;
-      }
-    };
-  },
+    getDefaultProps: function() {
+        return {
+            transformer: function(code) {
+                return JSXTransformer.transform(code, { harmony: true }).code;
+            }
+        };
+    },
 
-  getInitialState: function() {
-    return {
-      code: this.props.codeText,
-      error: null
-    };
-  },
+    getInitialState: function() {
+        return {
+            code: this.props.codeText,
+            error: null
+        };
+    },
 
-  handleCodeChange: function(value) {
-    this.setState({code: value});
-    this.executeCode();
-  },
+    handleCodeChange: function(value) {
+        this.setState({code: value});
+        this.executeCode();
+    },
 
-  compileCode: function() {
-    return this.props.transformer(this.state.code);
-  },
+    compileCode: function() {
+        return this.props.transformer(this.state.code);
+    },
 
-  render: function() {
-    var compiledCode = '';
-    try {
-      compiledCode = this.compileCode();
-    } catch (err) {}
+    render: function() {
+        var compiledCode = '';
+        try {
+            compiledCode = this.compileCode();
+        } catch (err) {}
 
-    var JSXContent =
-      React.createElement(CodeMirrorEditor, {
-        key: "jsx",
-        onChange: this.handleCodeChange,
-        className: "playgroundStage",
-        codeText: this.state.code
-      });
+        var JSXContent =
+            React.createElement(CodeMirrorEditor, {
+                key: "jsx",
+                onChange: this.handleCodeChange,
+                className: "playgroundStage",
+                codeText: this.state.code
+            });
 
-    return (
-      React.createElement("div", {className: "playground"},
-        React.createElement("div", {className: "playgroundCode"},
-          JSXContent
-        ),
-        React.createElement("div", {className: "playgroundPreview"},
-          React.createElement("div", {ref: "mount"}),
-          this.state.error && React.createElement("div", {className: "playgroundError"}, this.state.error)
-        )
-      )
-    );
-  },
+        return (
+            React.createElement("div", {className: "playground"},
+                React.createElement("div", {className: "playgroundCode"},
+                    JSXContent
+                ),
+                React.createElement("div", {className: "playgroundPreview"},
+                    React.createElement("div", {ref: "mount"}),
+                    this.state.error && React.createElement("div", {className: "playgroundError"}, this.state.error)
+                )
+            )
+        );
+    },
 
-  componentDidMount: function() {
-    // Note: we also inject a fake hook for this to work
-    this.makeHot = ReactHotAPI(this.getMountedInstances);
-    this.executeCode();
-  },
+    componentDidMount: function() {
+        // Note: we also inject a fake hook for this to work
+        this.makeHot = ReactHotAPI(this.getMountedInstances);
+        this.executeCode();
+    },
 
-  componentDidUpdate: function(prevProps, prevState) {
-    if (this.state.code !== prevState.code) {
-      this.executeCode();
+    componentDidUpdate: function(prevProps, prevState) {
+        if (this.state.code !== prevState.code) {
+            this.executeCode();
+        }
+    },
+
+    getMountedInstances: function () {
+        return [this.refs.mount.getDOMNode()];
+    },
+
+    executeCode: function() {
+        var mountNode = this.refs.mount.getDOMNode();
+
+        try {
+            var module = {};
+            var compiledCode = this.compileCode();
+            var makeHot = this.makeHot;
+
+            eval(compiledCode);
+            eval([
+                'if (module.exports) {',
+                '  module.exports = makeHot(module.exports, "module.exports");',
+                '  React.render(React.createElement(module.exports), mountNode);',
+                '}'
+            ].join('\n'));
+            this.setState({
+                error: null
+            });
+        } catch (err) {
+            this.setState({
+                error: err.toString()
+            });
+        }
     }
-  },
-
-  getMountedInstances: function () {
-    return [this.refs.mount.getDOMNode()];
-  },
-
-  executeCode: function() {
-    var mountNode = this.refs.mount.getDOMNode();
-
-    try {
-      var module = {};
-      var compiledCode = this.compileCode();
-      var makeHot = this.makeHot;
-
-      eval(compiledCode);
-      eval([
-        'if (module.exports) {',
-        '  module.exports = makeHot(module.exports, "module.exports");',
-        '  React.render(React.createElement(module.exports), mountNode);',
-        '}'
-      ].join('\n'));
-      this.setState({
-        error: null
-      });
-    } catch (err) {
-      this.setState({
-        error: err.toString()
-      });
-    }
-  }
 });
