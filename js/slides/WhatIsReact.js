46909949f8cdc6d404b162243e6bba68adb28de8
Alexis Leroux-Chartre <alexis.leroux.chartre@gmail.com>
2015-03-15 22:10:17 -0400

    Add bullet points on WhatIsReact

--- js/slides/WhatIsReact.js
+++ js/slides/WhatIsReact.js
@@ -5,6 +5,13 @@ module.exports = React.createClass({
         return (
             <section>
                 <h2>What is <em>React</em>{"?"}</h2>
+                <div>Library, not a <del>framework</del></div>
+                <div>Only cares about the UI</div>
+                <div>The "V" in MVC</div>
+                <div>28kb</div>
+                <div>Battle-tested on:</div>
+                <div><small>facebook, instagram</small></div>
+                <div><small>yahoo!, khanacademy, airbnb, atom</small></div>
             </section>
         );
     }
