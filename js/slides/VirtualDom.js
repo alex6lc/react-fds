f623d32ca2f1ecfa43c51e2fe078a5538e63e7a1
alex6lc <alexis.leroux.chartre@gmail.com>
2015-04-07 16:06:29 -0400

    Add notes for VirtualDOM

--- js/slides/VirtualDom.js
+++ js/slides/VirtualDom.js
@@ -5,6 +5,17 @@ module.exports = React.createClass({
         return (
             <section>
                 <h2>Virtual DOM</h2>
+                <aside className="notes">
+                    botleneck de performance d’une application web === DOM
+                    <br/>
+                    mutations dans le DOM ça coûte chère
+                    <br/>
+                    Style calculations, layout, paint, compositing
+                    <br/>
+                    mutations arrivés l’une après l’autre (cascade)
+                    <br/>
+                    garde en mémoire une représentation de qu’est que le DOM ressembler
+                </aside>
             </section>
         );
     }
